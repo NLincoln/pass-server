40c49237a7126abb7d7bb73b699a2d037e989390
nlincoln <nlincoln@intellifarms.com>
2016-10-12 11:16:14 -0500

    Initial commit of socket use

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from http.server import HTTPServer
-from server import RequestHandler
+from server import Server
 
 server_addr = ('127.0.0.1', 8081)
 print("Starting server on {}:{}".format(*server_addr))
 
-httpd = HTTPServer(server_address=server_addr, RequestHandlerClass=RequestHandler)
+httpd = Server(bind_options=server_addr)
 
 print('Server has started!')
 
+
+def generate_response(request):
+    print(request.decode('utf8'))
+    return '''\
+HTTP/1.1 200 OK
+
+{}'''.format(request)
+
+
 while True:
-    httpd.handle_request()
+    httpd.handle_request(generate_response)
--- server/__init__.py
+++ server/__init__.py
@@ -1,43 +1,17 @@
-from http.server import BaseHTTPRequestHandler
-import json
-import re
+import socket
 
 
-def url(method, route, callback):
-    return method, re.compile(route), callback
+class Server:
+    def __init__(self, bind_options=('127.0.0.1', 8888)):
+        self.listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listen_socket.bind(bind_options)
+        self.listen_socket.listen(1)
 
+    def handle_request(self, callback):
+        client_connection, client_address = self.listen_socket.accept()
+        request = client_connection.recv(1024)
+        response = callback(request)
 
-def handle_test(request):
-    return {'Message': request}
-
-
-urls = [
-    url('GET', r'^/test', handle_test)
-]
-
-
-class RequestHandler(BaseHTTPRequestHandler):
-    def create_request_object(self):
-        return {
-            "verb": self.command,
-            "path": self.path
-        }
-
-    def unknown_route_error(self):
-        return {"error": "Unknown route {}".format(self.path)}
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/json')
-        self.end_headers()
-
-        response = self.unknown_route_error()
-        for url in urls:
-            verb, regex, callback = url
-            if regex.match(self.path) and self.command == verb:
-                response = callback(self.create_request_object())
-                break
-
-        message = json.dumps(response)
-
-        self.wfile.write(bytes(message, "utf8"))
+        client_connection.sendall(bytes(response, 'utf8'))
+        client_connection.close()
